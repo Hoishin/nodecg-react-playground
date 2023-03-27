a289e81ef2a4e732e288043d9884d5c90494e4cb
Keiichiro Amemiya <hoishinxii@gmail.com>
2023-03-28 08:43:12 +0900

    throttle

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	root: true,
 	parser: "@typescript-eslint/parser",
 	plugins: ["react-refresh", "import"],
+	extends: ["plugin:react-hooks/recommended"],
 	rules: {
 		"react-refresh/only-export-components": "error",
 		"import/order": [
--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,5 +1,6 @@
+import { throttle } from "@github/mini-throttle";
 import Box from "@mui/material/Box";
-import { FC, useState } from "react";
+import { FC, useCallback, useLayoutEffect, useState } from "react";
 
 import { useElementResize } from "../hooks/useElementResize";
 
@@ -33,88 +34,107 @@ const Workspace: FC<Props> = (props) => {
 	const [containerRef, { width: containerWidth }] = useElementResize();
 	const [panelSizes, setPanelSizes] = useState<Size[]>([]);
 
-	const panelPositions: PanelPosition[] = [];
-	const corners: Coordinate[] = [{ x: 0, y: 0 }];
+	const [panelPositions, setPanelPositions] = useState<PanelPosition[]>([]);
+	const [corners, setCorners] = useState<Coordinate[]>([]);
 
-	const addCorner = (x: number, y: number) => {
-		const alreadyExists = corners.some(
-			(corner) => corner.x === x && corner.y === y
-		);
-		if (alreadyExists) {
-			return;
-		}
-		if (containerWidth <= x) {
-			return;
-		}
-		for (const position of panelPositions) {
-			const insidePanel =
-				position.left - GAP < x &&
-				x < position.right + GAP &&
-				position.top - GAP < y &&
-				y < position.bottom + GAP;
-			if (insidePanel) {
-				return;
-			}
-		}
-		corners.push({ x, y });
-	};
+	const calculatePanelPositions = useCallback(
+		throttle((panelSizes: Size[], containerWidth: number) => {
+			const _panelPositions: PanelPosition[] = [];
+			const _corners: Coordinate[] = [{ x: 0, y: 0 }];
 
-	for (const size of panelSizes) {
-		const fittingCorner = [...corners]
-			.sort((a, b) => {
-				return a.y - b.y || a.x - b.x;
-			})
-			.find((corner) => {
-				const tmpRight = corner.x + size.width;
-				const tmpBottom = corner.y + size.height;
-				if (containerWidth < tmpRight && corner.x !== 0) {
-					return false;
-				}
-				const overlapsWithOtherPanel = panelPositions.some(
-					(position) =>
-						position.left - GAP < tmpRight &&
-						corner.x < position.right + GAP &&
-						position.top - GAP < tmpBottom &&
-						corner.y < position.bottom + GAP
+			const addCorner = (x: number, y: number) => {
+				const alreadyExists = _corners.some(
+					(corner) => corner.x === x && corner.y === y
 				);
-				return !overlapsWithOtherPanel;
-			});
+				if (alreadyExists) {
+					return;
+				}
+				if (containerWidth <= x) {
+					return;
+				}
+				for (const position of _panelPositions) {
+					const insidePanel =
+						position.left - GAP < x &&
+						x < position.right + GAP &&
+						position.top - GAP < y &&
+						y < position.bottom + GAP;
+					if (insidePanel) {
+						return;
+					}
+				}
+				_corners.push({ x, y });
+			};
 
-		if (!fittingCorner) {
-			continue;
-		}
+			for (const size of panelSizes) {
+				const fittingCorner = [..._corners]
+					.sort((a, b) => {
+						return a.y - b.y || a.x - b.x;
+					})
+					.find((corner) => {
+						const tmpRight = corner.x + size.width;
+						const tmpBottom = corner.y + size.height;
+						if (containerWidth < tmpRight && corner.x !== 0) {
+							return false;
+						}
+						const overlapsWithOtherPanel = _panelPositions.some(
+							(position) =>
+								position.left - GAP < tmpRight &&
+								corner.x < position.right + GAP &&
+								position.top - GAP < tmpBottom &&
+								corner.y < position.bottom + GAP
+						);
+						return !overlapsWithOtherPanel;
+					});
 
-		const top = fittingCorner.y;
-		const left = fittingCorner.x;
-		const bottom = top + size.height;
-		const right = left + size.width;
+				if (!fittingCorner) {
+					continue;
+				}
 
-		const rightPanels = panelPositions
-			.filter((position) => right < position.right && position.bottom < bottom)
-			.sort((a, b) => b.bottom - a.bottom);
-		let maxX = Infinity;
-		for (const rightPanel of rightPanels) {
-			if (rightPanel.left < maxX) {
-				maxX = rightPanel.left;
-				addCorner(right + GAP, rightPanel.bottom + GAP);
+				const top = fittingCorner.y;
+				const left = fittingCorner.x;
+				const bottom = top + size.height;
+				const right = left + size.width;
+
+				const rightPanels = _panelPositions
+					.filter(
+						(position) => right < position.right && position.bottom < bottom
+					)
+					.sort((a, b) => b.bottom - a.bottom);
+				let maxX = Infinity;
+				for (const rightPanel of rightPanels) {
+					if (rightPanel.left < maxX) {
+						maxX = rightPanel.left;
+						addCorner(right + GAP, rightPanel.bottom + GAP);
+					}
+				}
+				addCorner(right + GAP, 0);
+
+				const belowPanels = _panelPositions
+					.filter(
+						(position) => bottom < position.bottom && position.right < right
+					)
+					.sort((a, b) => b.right - a.right);
+				let maxY = Infinity;
+				for (const belowPanel of belowPanels) {
+					if (belowPanel.top < maxY) {
+						maxY = belowPanel.top;
+						addCorner(belowPanel.right + GAP, bottom + GAP);
+					}
+				}
+				addCorner(0, bottom + GAP);
+
+				_panelPositions.push({ top, left, bottom, right });
 			}
-		}
-		addCorner(right + GAP, 0);
 
-		const belowPanels = panelPositions
-			.filter((position) => bottom < position.bottom && position.right < right)
-			.sort((a, b) => b.right - a.right);
-		let maxY = Infinity;
-		for (const belowPanel of belowPanels) {
-			if (belowPanel.top < maxY) {
-				maxY = belowPanel.top;
-				addCorner(belowPanel.right + GAP, bottom + GAP);
-			}
-		}
-		addCorner(0, bottom + GAP);
+			setPanelPositions(_panelPositions);
+			setCorners(_corners);
+		}, 200),
+		[]
+	);
 
-		panelPositions.push({ top, left, bottom, right });
-	}
+	useLayoutEffect(() => {
+		calculatePanelPositions(panelSizes, containerWidth);
+	}, [panelSizes, containerWidth, calculatePanelPositions]);
 
 	return (
 		<Box ref={containerRef} position="relative" width="100%" height="100%">
